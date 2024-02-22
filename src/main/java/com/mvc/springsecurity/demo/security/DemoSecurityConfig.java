0aa53cdd37f73a07dd432407e247e273ed2fec23
Vivek <vivek.agrahari@xcelore.com>
2024-02-22 19:03:01 +0530

    Restrict Access based roles

--- src/main/java/com/mvc/springsecurity/demo/security/DemoSecurityConfig.java
+++ src/main/java/com/mvc/springsecurity/demo/security/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.mvc.springsecurity.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,25 +23,33 @@ public class DemoSecurityConfig {
         UserDetails sunil = User.builder()
                 .username("sunil")
                 .password("{noop}test123")
-                .roles("EMPLOYEE, MANAGER")
+                .roles("EMPLOYEE", "MANAGER")
+               // .roles("EMPLOYEE, MANAGER")    // It will give run time error, white label
                 .build();
         UserDetails gopesh = User.builder()
                 .username("gopesh")
                 .password("{noop}test123")
-                .roles("EMLPOYEE, MANAGER, ADMIN")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(vivek, sunil, gopesh);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
-                .formLogin(form -> form.loginPage("/showMyLoginPage")
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
                         .loginProcessingUrl("/authenticateTheUser")
                         .permitAll())
                 .logout(logout->logout.permitAll()
                 );
 
+//        http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 }
